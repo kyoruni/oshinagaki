6dc6d3ed0cab20b63f1ade58143bfa9cfeb395f6
kyoruni <kyoruni@gmail.com>
2020-02-23 19:02:53 +0900

    :wrench: セクション分け

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -3,7 +3,12 @@
     <b-row>
       <div class="left col-8">
         <div class="preview m-4">
-          preview
+          <div class="preview__header">
+            header
+          </div>
+          <div class="preview__body">
+            body
+          </div>
         </div>
       </div>
       <div class="right col-4">
@@ -22,7 +27,17 @@ export default {
 <style lang="scss" scoped>
 .preview {
   background: pink;
-  border: 1px solid #333;
-  height: 800px;
+  height: 600px;
+  &__header {
+    background: lime;
+    border: 1px solid #333;
+    height: 100px;
+  }
+  &__body {
+    background: aqua;
+    border: 1px solid #333;
+    border-top: none;
+    height: 500px;
+  }
 }
 </style>
