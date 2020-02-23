12fd776809edcd7a9387ab94583585d006805f44
kyoruni <kyoruni@gmail.com>
2020-02-23 19:30:07 +0900

    :wrench: プレビュークリックで、メニューの表示切り替え

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -3,16 +3,26 @@
     <b-row>
       <div class="left col-8">
         <div class="preview m-4">
-          <div class="preview__header">
+          <div class="preview__header" @click="clickHeader">
             header
           </div>
-          <div class="preview__body">
+          <div class="preview__body" @click="clickBody">
             body
           </div>
         </div>
       </div>
       <div class="right col-4">
-        right
+        <div class="menu mt-4">
+          <div class="menu__default" v-if="showDefault">
+            default
+          </div>
+          <div class="menu__header" v-if="showMenuHeader">
+            header
+          </div>
+          <div class="menu__body" v-if="showMenuBody">
+            body
+          </div>
+        </div>
       </div>
     </b-row>
   </div>
@@ -20,14 +30,37 @@
 
 <script>
 export default {
-  name: 'Edit'
+  name: 'Edit',
+  data () {
+    return {
+      currentSection: 'default'
+    }
+  },
+  methods: {
+    clickHeader () {
+      this.currentSection = 'header';
+    },
+    clickBody () {
+      this.currentSection = 'body';
+    }
+  },
+  computed: {
+    showDefault () {
+      return this.currentSection === 'default';
+    },
+    showMenuHeader () {
+      return this.currentSection === 'header';
+    },
+    showMenuBody () {
+      return this.currentSection === 'body';
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .preview {
   background: pink;
-  height: 600px;
   &__header {
     background: lime;
     border: 1px solid #333;
@@ -39,5 +72,9 @@ export default {
     border-top: none;
     height: 500px;
   }
+}
+
+.menu {
+  background: yellow;
 }
 </style>
