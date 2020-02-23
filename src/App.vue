42c17c2972953490fdbf178aabd98ce6211e3536
kyoruni <kyoruni@gmail.com>
2020-02-23 13:01:15 +0900

    :wrench: font-awesome適用確認

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <font-awesome-icon :icon="['fas', 'user-secret']" />
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand href="#">お品書きメーカー</b-navbar-brand>
     </b-navbar>
