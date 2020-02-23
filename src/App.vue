9bd2199f0e2579ee17c0a8eb743c163c8a6577f3
kyoruni <kyoruni@gmail.com>
2020-02-23 13:43:24 +0900

    :wrench: トップページ作成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="lg" type="dark" class="header-nav">
       <b-navbar-brand to="/">
         <font-awesome-icon icon="pen" />
         お品書きメーカー
         </b-navbar-brand>
     </b-navbar>
     <b-container fluid>
-      <b-row>
-        <router-view/>
-      </b-row>
+      <router-view/>
     </b-container>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.header-nav {
+  background: #f98d6c;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
-    home
+    <b-row>
+      <!-- インフォメーション -->
+      <div class="infomation col-8 mt-4 mx-auto">
+        <p class="text-center m-4">
+          <span class="font-weight-bold">
+            即売会向けお品書き画像作成サイト<br />
+          </span>
+          動作確認済：Chrome
+        </p>
+      </div>
+      <!-- お知らせ -->
+    </b-row>
   </div>
 </template>
 
@@ -10,3 +21,11 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+.infomation {
+  background: #f98d6c;
+  border-radius: 10px;
+  color: #fff;
+}
+</style>
