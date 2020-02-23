7b21c0a48c60bd97d0f06ec99c0bc7d6207547ad
kyoruni <kyoruni@gmail.com>
2020-02-23 18:41:34 +0900

    :wrench: 2カラムレイアウトにする

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="edit">
-    edit
+    <b-row class="mt-4">
+      <div class="left col-8 bg-warning">
+        left
+      </div>
+      <div class="right col-4 bg-danger">
+        right
+      </div>
+    </b-row>
   </div>
 </template>
 
