7965db5ed37affa8244d68ddb4591c986dac40c9
kyoruni <kyoruni@gmail.com>
2020-02-23 15:56:26 +0900

    :wrench: インフォメーション表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,14 @@
     <!-- インフォメーション -->
     <b-row class="mt-4">
       <div class="infomation col-8 mx-auto">
-        <p class="text-center m-4">
-          <span class="font-weight-bold">
+        <div class="text-center m-4">
+          <p class="infomation--title">
             即売会向けお品書き画像作成サイト<br />
+          </p>
+          <span class="infomation--operation-check">
+            動作確認済：Chrome
           </span>
-          動作確認済：Chrome
-        </p>
+        </div>
       </div>
     </b-row>
     <!-- お知らせ -->
@@ -28,9 +30,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// インフォメーション
 .infomation {
   background: #f98d6c;
   border-radius: 5px;
   color: #fff;
+  // インフォメーションのタイトル
+  &--title {
+    font-weight: bold;
+    margin-bottom: 6px;
+  }
 }
 </style>
