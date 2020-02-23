1a6abde9394185f1737e822d53b20596969a5995
kyoruni <kyoruni@gmail.com>
2020-02-23 13:05:20 +0900

    :wrench: ヘッダーにアイコン表示

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <font-awesome-icon :icon="['fas', 'user-secret']" />
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">お品書きメーカー</b-navbar-brand>
+      <b-navbar-brand to="/">
+        <font-awesome-icon icon="pen" />
+        お品書きメーカー
+        </b-navbar-brand>
     </b-navbar>
     <router-view/>
   </div>
