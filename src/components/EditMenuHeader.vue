a828a6a58e669ffa756f678ec57e21b6738acb44
kyoruni <kyoruni@gmail.com>
2020-03-02 19:47:55 +0900

    :wrench: ヘッダーの入力フォーム作成

--- src/components/EditMenuHeader.vue
+++ src/components/EditMenuHeader.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="edit-menu-header">
-    header
+    <!-- 日付 -->
+    <div class="form-group">
+      <label for="inputEventDate">イベント開催日</label>
+      <input type="text" class="form-control" id="inputEventDate" placeholder="イベント開催日">
+    </div>
+    <!-- イベント名 -->
+    <div class="form-group">
+      <label for="inputEventName">イベント名</label>
+      <input type="text" class="form-control" id="inputEventName" placeholder="イベント名">
+    </div>
+    <!-- スペース名 -->
+    <div class="form-group">
+      <label for="inputSpaceName">スペース名</label>
+      <input type="text" class="form-control" id="inputSpaceName" placeholder="スペース名">
+    </div>
+    <!-- サークル名 -->
+    <div class="form-group">
+      <label for="inputCircleName">サークル名</label>
+      <input type="text" class="form-control" id="inputCircleName" placeholder="サークル名">
+    </div>
   </div>
 </template>
 
