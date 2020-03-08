a20ecafea8be1b1b5ed91e5e63b3c0f267765a8e
kyoruni <kyoruni@gmail.com>
2020-03-08 22:01:20 +0900

    :wrench: 日付をカレンダーから選択できるように

--- src/components/EditMenuHeader.vue
+++ src/components/EditMenuHeader.vue
@@ -3,13 +3,16 @@
     <!-- 日付 -->
     <div class="form-group">
       <label for="inputEventDate">イベント開催日</label>
-      <input
+      <!-- <input
         type="text"
         class="form-control"
         id="inputEventDate"
         placeholder="イベント開催日"
         :value="eventDate"
-        @input="setEventDate">
+        @input="setEventDate"> -->
+        <DatePicker
+          v-model="date"
+          @input="setEventDate"/>
     </div>
     <!-- イベント名 -->
     <div class="form-group">
@@ -48,11 +51,21 @@
 </template>
 
 <script>
+import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+
 export default {
   name: 'EditMenuHeader',
+  components: {
+    DatePicker
+  },
+  data () {
+    return {
+      date: new Date(),
+    }
+  },
   methods: {
     setEventDate (value) {
-      this.$emit('eventDate', value.target.value)
+      this.$emit('eventDate', value)
     },
     setEventName (value) {
       this.$emit('eventName', value.target.value)
@@ -65,7 +78,6 @@ export default {
     }
   },
   props: [
-    'eventDate',
     'eventName',
     'spaceName',
     'circleName'
